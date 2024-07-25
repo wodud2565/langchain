4c8fac477b6eed9fac311ebd26bdea5bfd5a245b
wodud2565 <147394883+wodud2565@users.noreply.github.com>
2024-07-26 01:27:59 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -21,11 +21,11 @@ import os
 
 def main():
     st.set_page_config(
-        page_title="carbot",
-        page_icon=":cars:"
+        page_title="DirChat",
+        page_icon="\U0001F697"  # 자동차 아이콘
     )
 
-    st.title("_VehicleChatbot :red[CarBot]_ :cars:")
+    st.title("_Private Data :red[QA Chat]_ \U0001F697")
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -65,7 +65,7 @@ def main():
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = [{"role": "assistant",
-                                         "content": "안녕하세요! 차량에 대해 궁금하신 것이 차량의 이름을 입력해주세요!"}]
+                                         "content": "안녕하세요! 주어진 문서에 대해 궁금하신 것이 있으면 언제든 물어봐주세요!"}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -94,107 +94,4 @@ def main():
 
                         st.markdown(response)
                         if st.session_state.car_data is not None:
-                            car_info = get_car_info(query)
-                            if car_info is not None:
-                                st.markdown("### 차량 정보")
-                                st.dataframe(car_info)
-                                photo_number = car_info['사진번호'].values[0]
-                                image_path = f"images/{photo_number}.png"
-                                logger.info(f"Looking for image at: {image_path}")
-                                if os.path.exists(image_path):
-                                    st.image(image_path, caption=f"차량 {photo_number}")
-                                else:
-                                    st.markdown("이미지를 찾을 수 없습니다.")
-
-                        with st.expander("참고 문서 확인"):
-                            for doc in source_documents:
-                                st.markdown(f"{doc.metadata['source']}", help=doc.page_content)
-
-                        # Add assistant message to chat history
-                        st.session_state.messages.append({"role": "assistant", "content": response})
-                    except Exception as e:
-                        st.error(f"An error occurred: {e}")
-            else:
-                st.error("Conversation chain is not initialized. Please process the documents first.")
-
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    tokens = tokenizer.encode(text)
-    return len(tokens)
-
-def get_text(docs):
-    doc_list = []
-
-    for doc in docs:
-        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-            file.write(doc.getvalue())
-            logger.info(f"Uploaded {file_name}")
-        if '.pdf' in doc.name:
-            loader = PyPDFLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.docx' in doc.name:
-            loader = Docx2txtLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.pptx' in doc.name:
-            loader = UnstructuredPowerPointLoader(file_name)
-            documents = loader.load_and_split()
-        elif '.csv' in doc.name:
-            df = pd.read_csv(file_name)
-            documents = CSVLoader(file_path=file_name).load()
-
-        doc_list.extend(documents)
-    return doc_list
-
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900,
-        chunk_overlap=100,
-        length_function=tiktoken_len
-    )
-    chunks = text_splitter.split_documents(text)
-    return chunks
-
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(
-        model_name="jhgan/ko-sroberta-multitask",
-        model_kwargs={'device': 'cpu'},
-        encode_kwargs={'normalize_embeddings': True}
-    )
-    vectordb = FAISS.from_documents(text_chunks, embeddings)
-    return vectordb
-
-def get_conversation_chain(vetorestore, openai_api_key):
-    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo', temperature=0)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        chain_type="stuff",
-        retriever=vetorestore.as_retriever(search_type='mmr', verbose=True),
-        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
-        get_chat_history=lambda h: h,
-        return_source_documents=True,
-        verbose=True
-    )
-
-    return conversation_chain
-
-def save_uploaded_images(uploaded_images):
-    if not os.path.exists("images"):
-        os.makedirs("images")
-    for img in uploaded_images:
-        img_path = os.path.join("images", img.name)
-        with open(img_path, "wb") as f:
-            f.write(img.getvalue())
-        logger.info(f"Uploaded image {img.name} to {img_path}")
-
-def get_car_info(query):
-    if st.session_state.car_data is not None:
-        df = st.session_state.car_data
-        # 쿼리를 기반으로 차량 정보를 검색합니다.
-        result = df[df.apply(lambda row: query.lower() in row.to_string().lower(), axis=1)]
-        if not result.empty:
-            return result
-    return None
-
-if __name__ == '__main__':
-    main()
+                            c
